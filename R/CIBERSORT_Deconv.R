b3c94b28138bbc2e78b624397d7e2428ad36438d
nidiabarcoarmengol <92799815+nidiabarcoarmengol@users.noreply.github.com>
2022-10-27 10:20:10 +0200

    Add files via upload

--- R/CIBERSORT_Deconv.R
+++ R/CIBERSORT_Deconv.R
@@ -2,9 +2,8 @@
 ##' @name CIBERSORT_Deconv
 ##' @alias CIBERSORT_Deconv
 ##' @title CIBERSORT_Deconv
-##' CIBERSORT_Deconv
 ##'
-##' Function that generates the relative or absolute deconvolution plots with the CIBERSORT method, with the non-fractional plot, returns a df resulting from the deconvolution and two graphs of deconvolution data.
+##' @usage Function that generates the relative or absolute deconvolution plots with the CIBERSORT method, with the non-fractional plot, returns a df resulting from the deconvolution and two graphs of deconvolution data.
 ##' @param matrix Parameter in which a matrix (in .txt, .csv or .tsv format) is introduced, which in the first column has the names of the genes and in the rest of the columns the expressions for each gene of the different samples.
 ##' @param sig.matrix Parameter in which a matrix (in .txt, .csv or .tsv format) is introduced, with the first column containing the names of the genes and the rest of the columns containing the expression signatures of different cell types.
 ##' @param method Parameter where you enter "abs" if you want to do the absolute deconvolution of the matrix, by default it is "rel".
@@ -14,15 +13,16 @@
 ##' @param height_heatmap Parameter in which a value is entered that determines the height of the heatmap, by default it is 700.
 ##' @param width_heatmap Parameter in which a value is entered that determines the width of the heatmap, by default it is 700.
 ##' @param name Parameter to enter the name you want to be included in the generated graphics (Ex. "LM22" -> "CIBERSORT_Deconv_LM22_rel_plot.png"), default would be ("CIBERSORT_Deconv_(sig.matrix)_rel_plot.png").
-##' @param number_format Parameter that allows to change the visualization of the numbers in the heatmap. "%.2f" for two decimals and "%.1e" for exponential notation.
+##' @param number_format Parameter that allows to change the visualization of the numbers in the heatmap. ("\%.2f") for two decimals and ("\%.1e") for exponential notation.
 ##' @param byCond Parameter in which TRUE is introduced if we introduce a vector so that the function generates the graphs dividing the samples by its condition, by default it is FALSE.
 ##' @param cond Vector that assigns a condition to each sample of the data frame.
+##' @param cibersortpath Directory where the document CIBERSORT.R is located.
 ##' @return Returns the generated graphs and the df of the deconvolution.
 ##' @author Nidia Barco Armengol
 ##' @export
 ##'
 ##' @examples
-##'
+##' c<- CIBERSORT_Deconv(matrix = matrix, sig.matrix = sig.matrix, results_dir = results_dir, cibersortpath = path_ciber, by_Cond = TRUE, cond = fractions, method = "abs")
 
 CIBERSORT_Deconv<- function(matrix, sig.matrix, method= "rel", results_dir, height_deconv= 10, width_deconv= 9, height_heatmap= 700, width_heatmap= 700, name= FALSE, number_format= "%.2f", by_Cond= FALSE, cond, cibersortpath){
   require(usethis)
@@ -36,6 +36,7 @@ CIBERSORT_Deconv<- function(matrix, sig.matrix, method= "rel", results_dir, heig
   require(RColorBrewer)
   source(file.path(cibersortpath, "CIBERSORT.R"))
   if (method=="rel"){
+    # The deconvolution is performed and gives results over 1.
     res_ciber = CIBERSORT(sig_matrix=sig.matrix,
                           mixture_file = matrix,
                           QN = F,perm=100)
@@ -43,6 +44,7 @@ CIBERSORT_Deconv<- function(matrix, sig.matrix, method= "rel", results_dir, heig
     res_ciber$cell_type=rownames(res_ciber)
     C <- res_ciber[1:(length(rownames(res_ciber))-3),]
   }else{
+    # The deconvolution is performed and gives quantitative results.
     res_ciber = CIBERSORT(sig_matrix=sig.matrix,
                           mixture_file = matrix,
                           QN = F,perm=100,absolute = T,abs_method='no.sumto1')
@@ -51,9 +53,9 @@ CIBERSORT_Deconv<- function(matrix, sig.matrix, method= "rel", results_dir, heig
     C <- res_ciber[1:(length(rownames(res_ciber))-4),]
   }
   if (by_Cond==FALSE){
+    # If there is no condition it does not loop to generate several graphs.
     if (name==FALSE){
-      title <- gsub(".txt","_",sig.matrix)
-      file <- c("CIBERSORT_fractions_", title , "_" , method , "_","plot.png")
+      file <- c("CIBERSORT_fractions_", method , "_","plot.png")
     }else{
       title <- name
       file <- c("CIBERSORT_fractions_", title , "_" , method , "_","plot.png")
@@ -62,8 +64,7 @@ CIBERSORT_Deconv<- function(matrix, sig.matrix, method= "rel", results_dir, heig
     C$cell_type=rownames(C)
     Deconvolution_graph(df= C, file = file, results_dir = results_dir, height = height_deconv, width = width_deconv)
     if (name==FALSE){
-      title <- gsub(".txt","_",sig.matrix)
-      file <- c("CIBERSORT_heatmap_", title , "_" , method , "_","plot.png")
+      file <- c("CIBERSORT_heatmap_", method , "_","plot.png")
     }else{
       title <- name
       file <- c("CIBERSORT_heatmap", title , "_" , method , "_","plot.png")
@@ -71,6 +72,7 @@ CIBERSORT_Deconv<- function(matrix, sig.matrix, method= "rel", results_dir, heig
     file <- paste(file, collapse = "")
     Heatmap_graph(df= C, file = file, results_dir = results_dir, number_format = number_format, width = width_heatmap, height = height_heatmap)
   }else{
+    # If there is a condition vector, the data enters a llop that divides the df and generates as many graphs of each type as there are conditions in the vector.
     list_pos <- list()
     unics <- unique(cond)
     for (i in unics){
@@ -96,8 +98,7 @@ CIBERSORT_Deconv<- function(matrix, sig.matrix, method= "rel", results_dir, heig
       colnames(df) <- df_names
 
       if (name==FALSE){
-        title <- gsub(".txt","_",sig.matrix)
-        file <- c("CIBERSORT_fractions_", title ,"_", method, "_", x, "_","plot.png")
+        file <- c("CIBERSORT_fractions_", method, "_", x, "_","plot.png")
       }else{
         title <- name
         file <- c("CIBERSORT_fractions_", title ,"_", method, "_", x, "_","plot.png")
@@ -106,8 +107,7 @@ CIBERSORT_Deconv<- function(matrix, sig.matrix, method= "rel", results_dir, heig
       c$cell_type=rownames(c)
       Deconvolution_graph(df= c, file = file, results_dir = results_dir, height = height_deconv, width = width_deconv)
       if (name==FALSE){
-        title <- gsub(".txt","_",sig.matrix)
-        file <- c("CIBERSORT_heatmap_", title ,"_", method, "_", x, "_","plot.png")
+        file <- c("CIBERSORT_heatmap_", method, "_", x, "_","plot.png")
       }else{
         title <- name
         file <- c("CIBERSORT_heatmap_", title ,"_", method, "_", x, "_","plot.png")
